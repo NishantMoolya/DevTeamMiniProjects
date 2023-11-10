ce18eff784f0cf422c4cf115991e0508cadcc866
NishantMoolya <nishantmoolya@gmail.com>
2023-11-10 22:55:06 +0530

    created images folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
-import { Box, Stack,ThemeProvider,createTheme} from '@mui/material';
+import { Stack, ThemeProvider, createTheme } from '@mui/material';
 import React from 'react';
-import "../styles/App.css";
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import Rightbar from './Rightbar';
@@ -8,22 +7,22 @@ import Content from './Content';
 import { useState } from 'react';
 
 const App = () => {
-  const [mode,setMode] = useState("light");
+  const [mode, setMode] = useState("light");
   const darkTheme = createTheme({
-    palette:{
-      mode:mode
+    palette: {
+      mode: mode
     }
   })
   return (
     <ThemeProvider theme={darkTheme}>
-    <Stack bgcolor="background.default" color="text.primary">
-      <Navbar mode={mode}/>
-      <Stack direction="row">
-      <Sidebar setMode={setMode} mode={mode}/>
-      <Content />
-      <Rightbar />
+      <Stack bgcolor="background.default" color="text.primary">
+        <Navbar mode={mode} />
+        <Stack direction="row">
+          <Sidebar setMode={setMode} mode={mode} />
+          <Content />
+          <Rightbar />
+        </Stack>
       </Stack>
-    </Stack>
     </ThemeProvider>
   )
 }
--- src/components/images.js
+++ src/components/images.js
@@ -1,10 +1,10 @@
-import pic1 from "../pic1.jpg";
-import pic2 from "../pic2.jpg";
-import pic3 from "../pic3.jpg";
-import pic4 from "../pic4.jpg";
-import pic5 from "../pic5.jpg";
-import pic6 from "../pic6.jpg";
-import pic7 from "../pic7.jpg";
+import pic1 from "../img/pic1.jpg";
+import pic2 from "../img/pic2.jpg";
+import pic3 from "../img/pic3.jpg";
+import pic4 from "../img/pic4.jpg";
+import pic5 from "../img/pic5.jpg";
+import pic6 from "../img/pic6.jpg";
+import pic7 from "../img/pic7.jpg";
 
 const images = [pic1,pic2,pic3,pic4,pic5,pic6,pic7];
 
